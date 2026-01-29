a9d8422c36e1254055e9813a63fecdf0c4bbc553
JMarin2001-adso <marinjhon002@gmail.com>
2026-01-28 23:15:31 -0500

    nuevo producto

--- assets/js/panel_rotacion.js
+++ assets/js/panel_rotacion.js
@@ -63,3 +63,113 @@ function cargarGrafica(data) {
         }
     });
 }
+
+//validacion empleado
+const empleadoId = localStorage.getItem("empleadoId");
+const empleadoNombre = localStorage.getItem("empleadoNombre");
+
+if (!empleadoId) {
+    window.location.href = "loginempleado.html";
+}
+
+//mostrar nombre
+document.getElementById("user-name").textContent = empleadoNombre;
+
+
+//Manejo menu hamburguesa
+const sidebar = document.getElementById("sidebar");
+document.getElementById("menu-toggle").addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+});
+
+
+// usuario-dopdow
+const userDropdown = document.getElementById("user-dropdown");
+document.getElementById("user-icon").addEventListener("click", () => {
+    userDropdown.classList.toggle("show");
+});
+
+
+//logout
+document.getElementById("logout-btn").addEventListener("click", () => {
+    localStorage.removeItem("empleadoId");
+    localStorage.removeItem("empleadoNombre");
+    localStorage.removeItem("empleadoRol");
+    window.location.href = "loginempleado.html";
+});
+
+// notificaiones-Drpdown
+const notifIcon = document.getElementById("notif-icon");
+const notifDropdown = document.getElementById("notif-dropdown");
+
+notifIcon.addEventListener("click", () => {
+    notifDropdown.classList.toggle("show");
+});
+
+//Cargar notificaciones
+async function cargarNotificaciones() {
+    try {
+        const res = await fetch(`${API_BASE}/pedido/pendientes`);
+        const pedidos = await res.json();
+
+        // Cantidad
+        document.getElementById("notif-count").textContent = pedidos.length;
+
+        // Lista
+        const notifList = document.getElementById("notif-list");
+        notifList.innerHTML = "";
+
+        pedidos.forEach(p => {
+            let li = document.createElement("li");
+            li.textContent = `Pedido #${p.id_pedido} en revisión`;
+            
+            li.addEventListener("click", () => {
+                window.location.href = `revision_pedido.html?id=${p.id_pedido}`;
+            });
+            
+            notifList.appendChild(li);
+        });
+
+
+    } catch (error) {
+        console.error("Error cargando notificaciones:", error);
+    }
+}
+
+// Ejecutar al cargar el panel
+cargarNotificaciones();
+
+// Consultar cada 8 segundos
+setInterval(cargarNotificaciones, 8000);
+
+
+//Navegacion entres paginas del sidebar
+
+document.querySelectorAll(".nav-item").forEach(item => {
+    item.addEventListener("click", (e) => {
+        e.preventDefault(); // evitar salto arriba
+
+        const destino = item.getAttribute("data-page");
+
+        if (destino) {
+            window.location.href = destino;
+        }
+    });
+});
+
+// Mostrar / ocultar submenú de Transacciones
+document.getElementById("btn-transacciones").addEventListener("click", () => {
+    const submenu = document.getElementById("submenu-transacciones");
+    submenu.style.display =
+        submenu.style.display === "block" ? "none" : "block";
+});
+
+// Navegación de submenú
+document.querySelectorAll(".nav-subitem").forEach(item => {
+    item.addEventListener("click", () => {
+        const page = item.getAttribute("data-page");
+        window.location.href = page;
+    });
+});
+
+
